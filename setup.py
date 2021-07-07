db6d7860f1212ba85faffa757fe74414c25942b2
Aditya Gupta <adigupta@seas.upenn.edu>
2021-07-07 16:00:52 -0400

    "working driver"

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='ceaos_reservoir',
+    name='ceaos_light',
     version='0.0.1',
-    packages=['ceaos_reservoir'],
+    packages=['ceaos_light'],
     install_requires=[
         'pigpio',
         'zmq',
